1f35b4d5d7f0534acfc0af4c09e2f462545f29d6
codemazui <codemazui@gmail.com>
2024-01-05 13:05:08 -0300

    up

---
+++ CS50P/ProblemSets/PSet7/working.py
@@ -0,0 +1,39 @@
+import re
+
+
+def main():
+    print(convert(input("Hours: ")))
+
+
+def convert(s):
+    am = {"12": "00", "1": "01", "2": "02", "3": "03", "4": "04", "5": "05", "6": "06", "7": "07", "8": "08", "9": "09", "10": "10", "11": "11"}
+    pm = {"12": "12", "1": "13", "2": "14", "3": "15", "4": "16", "5": "17", "6": "18", "7": "19", "8": "20", "9": "21", "10": "22", "11": "23"}
+    start, end = s.split(" to ")
+    start = conv_parts(start, am, pm)
+    end = conv_parts(end, am, pm)
+    return f"{start} to {end}"
+
+
+def conv_parts(part, am, pm):
+    if matches := re.search(r"(\d{1,2})(:\d{2})? (A|P)M", part):
+        hr = matches.group(1)
+        ap = matches.group(3)
+        if min := matches.group(2):
+            min = min[1:]
+        else:
+            min = "00"
+        if int(hr) <= 0 or int(hr) > 12:
+            raise ValueError("Invalid hr")
+        elif int(min) < 0 or int(min) > 59:
+            raise ValueError("Invalid min")
+        elif ap == "A":
+            hr = am[hr]
+        elif ap == "P":
+            hr = pm[hr]
+        return f"{hr}:{min}"
+    else:
+        raise ValueError("no matches s")
+
+
+if __name__ == "__main__":
+    main()
