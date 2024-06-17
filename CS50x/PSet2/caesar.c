4b62cc999fa224472c9a2669024512f70a411572
CodeMazui <137234453+codemazui@users.noreply.github.com>
2024-06-17 10:28:35 -0300

    up

--- CS50x/PSet2/caesar.c
+++ CS50x/PSet2/caesar.c
@@ -3,40 +3,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+bool only_digits(string s);
+char rotate(char c, int n);
+
 int main(int argc, string argv[]) {
     // Make sure every character in argv[1] is a digit
     if (argc != 2) {
         printf("Usage: ./ceasar key\n");
         return 1;
     }
-    for (int i = 0; argv[1][i] != '\0'; ++i) {
-        if (isdigit(argv[1][i]) == 0) {
-            printf("Usage: ./ceasar key\n");
-            return 1;
-        }
+    if (only_digits(argv[1]) == false) {
+        printf("Usage: ./ceasar key\n");
+        return 1;
     }
 
     // Convert argv[1] from a `string` to an `int`
     int key = atoi(argv[1]);
 
     // Prompt user for plaintext
-    string plaintext = get_string("plaintext: ");
+    string plaintext = get_string("plaintext:  ");
 
-    printf("ciphertext:  ");
+    printf("ciphertext: ");
 
     // For each character in the plaintext:
     for (int i = 0; plaintext[i] != '\0'; i++) {
         // Rotate the character if it's a letter
-        if (isalpha(plaintext[i]) != 0) {
-            if (isupper(plaintext[i])) {
-                printf("%c", ((plaintext[i] - 'A' + key) % 26) + 'A');
-            } else {
-                printf("%c", ((plaintext[i] - 'a' + key) % 26) + 'a');
-            }
-        } else {
-            printf("%c", plaintext[i]);
-        }
+        printf("%c", rotate(plaintext[i], key));
     }
     printf("\n");
     return 0;
+}
+
+bool only_digits(string s) {
+    for (int i = 0; s[i] != '\0'; ++i) {
+        if (isdigit(s[i]) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+char rotate(char c, int k) {
+    if (isalpha(c) != 0) {
+        if (isupper(c)) {
+            return (((c - 'A' + k) % 26) + 'A');
+        } else {
+            return (((c - 'a' + k) % 26) + 'a');
+        }
+    } else {
+        return c;
+    }
 }
